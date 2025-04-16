c367c6525909cd839e270822d9d4b7476ff452ef
govind2988 <govindarajp@xduce.com>
2025-04-16 18:10:40 +0530

    Login Model

--- reactjs19/src/components/common/home/Navbar.jsx
+++ reactjs19/src/components/common/home/Navbar.jsx
@@ -1,40 +1,18 @@
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Login from "./Login"; // Import the Login component
+
 const NavbarHome = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          Home
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/info">
-                Info
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/portal">
-                Portal
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
-
       <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
         <div className="container px-5">
-          <a className="navbar-brand" href="index.html">
+          <a className="navbar-brand" href="/">
             <span className="fw-bolder text-primary">Start Bootstrap</span>
           </a>
           <button
@@ -65,10 +43,17 @@ const NavbarHome = () => {
                   Portal
                 </a>
               </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={handleShow}>
+                  Login
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
+      <Login show={show} handleClose={handleClose} />{" "}
+      {/* Use the Login component */}
     </>
   );
 };
